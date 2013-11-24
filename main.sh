6dc5687e58696a507a72be08b977cf2861476330
Shubhamoy <shubhamoy@kalakhatta.com>
2013-11-25 00:56:07 +0530

    Added MongoDB Support

--- main.sh
+++ main.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-if sudo apt-get -y install python-software-properties && sudo add-apt-repository -y ppa:nginx/stable && sudo apt-get update && sudo apt-get -y install nginx && sudo service nginx start
+echo "*********************************************"
+echo " PHP + MySQL + nGinx :: Installation Script"
+echo "*********************************************"
+
+if sudo apt-get -y install python-software-properties && sudo add-apt-repository -y ppa:nginx/stable && echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list  && sudo apt-get update && sudo apt-get -y install nginx && sudo service nginx start
 then
-  nginx_info="Nginx Stable version Installed and is Running."
+  nginx_info="nGinx Stable Version Installed and is up running!!"
 else
-  nginx_info="Problem with Packages"
+  nginx_info="Problem Installing nGinx"
 fi
 
-#this is to change nginx root directory
+echo "************************************"
+echo "Changing nGinx Server Root Directory"
+echo "************************************"
 if sed -i 's/root \/usr\/share\/nginx\/html;/root \/home\/www;/g' /etc/nginx/sites-available/default && sed -i 's/index index.html index.htm;/index index.html index.htm index.php;/g' /etc/nginx/sites-available/default 
 then
-  nginx_dir="Nginx default server directory changed to /home/www"
+  nginx_dir="nGinx Default Server Directory Changed to /home/www"
 else
-  nginx_dir="Could not change nginx default server directory"
+  nginx_dir="Couldn't Change nGinx Default Server Directory"
 fi
 
-#Installing PHP
-if add-apt-repository -y ppa:ondrej/php5 && apt-get -y update && apt-get install -y php5-fpm php5-mcrypt php5-cli
+echo "**************"
+echo "Installing PHP"
+echo "**************"
+if add-apt-repository -y ppa:ondrej/php5 && apt-get -y update && apt-get install -y php5-fpm php5-mcrypt php5-cli php5-dev php-pear
 then
   php_info="PHP Installed"
 else
@@ -23,24 +31,27 @@ else
 fi
 
 #Database Install
-echo "What database do you want to Install, Type mysql for Mysql and mongo for MongoDB: "
+echo "Which database engine do you want to Install? Type mysql for MySQL and mongo for MongoDB: "
 read input
 if [ "$input" == "mysql" ];
 then
-  printf "\nMysql is selected and installing.\n\n"
+  printf "\nMySQL is selected and Installing.\n\n"
   sleep 0.9
-  mysql_info="Mysql is installed with empty root password. You have to set it manually"
+  mysql_info="MySQL is installed with empty root password. You have to set it manually"
   export DEBIAN_FRONTEND=noninteractive
   apt-get -q -y install mysql-server mysql-client php5-mysql
 elif [ "$input" == "mongo" ];
 then
   printf "\n MongoDB Selected.\n\n"  
+  apt-get -q -y install mongodb-10gen
+  pecl install mongo
+  echo extension=mongo.so>>/etc/php5/fpm/php.ini
 fi
 
 #Uncomment Few Lines from Virtual Host
 if wget "https://raw.github.com/AbhishekGahlot/Ubuntu-Nginx-Mysql-PHP/master/regex_abhishek.py" && python regex_abhishek.py && service nginx restart && mkdir /home/www
 then
-	final_info="PHP is now working with nginx. Create php file in /home/www and execute in browser"
+	final_info="PHP is now working with nGinx. Create php file in /home/www and execute in browser"
 	rm regex_abhishek.py
 else
 	final_info="Problem fetching file, You will have to configure virtual host manually"
